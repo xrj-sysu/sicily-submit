50a0ad87919240da1749b9fda9a4e0ec3addcff9
xrj-sysu <xrj_sysu@foxmail.com>
2014-10-11 22:14:20 +0800

    Create 基于数组的栈.cpp

---
+++ 基于数组的栈.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+enum ErrorCode {
+    success,
+    underflow,
+    overflow
+};
+const int maxStack = 100;
+
+template <class StackEntry>
+class MyStack {
+    public:
+        MyStack();
+        bool empty() const;
+        ErrorCode pop();
+        ErrorCode top(StackEntry &item) const;
+        ErrorCode push(const StackEntry &item);
+    private:
+        int size;
+        StackEntry entry[maxStack];
+};
+template <class StackEntry>
+MyStack<StackEntry>::MyStack() {
+	size = 0;
+}
+template <class StackEntry>
+bool MyStack<StackEntry>::empty() const {
+    return size == 0;
+}
+template <class StackEntry>
+ErrorCode MyStack<StackEntry>::pop() {
+	if (size == 0) {
+		return underflow;
+	}
+	size--;
+	return success;
+}
+template <class StackEntry>
+ErrorCode MyStack<StackEntry>::top(StackEntry &item) const {
+    if (size == 0) {
+    	return underflow;
+    }
+    item = entry[size-1];
+    return success;
+}
+template <class StackEntry>
+ErrorCode MyStack<StackEntry>::push(const StackEntry &item) {
+	if (size >= 100) {
+		return overflow;
+	}
+	entry[size] = item;
+	size++;
+	return success;
+}
+int main() {
+	MyStack<int> s;
+	int temp;
+	cout << s.empty() << endl;
+	s.push(1);
+	cout << s.empty() << endl;
+	cout << s.push(2) << endl;
+	s.push(3);
+	s.top(temp);
+	cout << temp << endl;
+	s.pop();
+	return 0;
+}
+
+
